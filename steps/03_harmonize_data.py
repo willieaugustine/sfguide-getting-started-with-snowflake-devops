48c5e819388a15ac7b9848db229e56212a6b844f
Willie <mulwawillie@gmail.com>
2025-05-24 13:28:14 +0000

    parametrize pipeline

--- steps/03_harmonize_data.py
+++ steps/03_harmonize_data.py
@@ -266,9 +266,6 @@ pipeline = [
 root = Root(Session.builder.getOrCreate())
 
 # create views in Snowflake
-silver_schema = root.databases["quickstart_prod"].schemas["silver"]
-silver_schema.user_defined_functions.create(
-    map_city_to_airport, mode=CreateMode.or_replace
-)
+silver_schema = silver_schema = root.databases[f"quickstart_{os.environ['environment']}"].schemas["silver"]
 for view in pipeline:
     silver_schema.views.create(view, mode=CreateMode.or_replace)
